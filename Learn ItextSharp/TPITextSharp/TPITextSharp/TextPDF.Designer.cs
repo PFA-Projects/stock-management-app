f92154f0fc8015dbba8dde2ecdf49e890c3cc98e
AitalMariam <Aital_1998@outlook.com>
2017-05-08 12:51:34 +0100

    itextsharp

---
+++ Learn ItextSharp/TPITextSharp/TPITextSharp/TextPDF.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TPITextSharp
+{
+    partial class TextPDF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextPDF));
+            this.msgpdf_btn = new System.Windows.Forms.Button();
+            this.GroupsDataGridView = new System.Windows.Forms.DataGridView();
+            this.data_btn = new System.Windows.Forms.Button();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.readpdf_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.GroupsDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msgpdf_btn
+            // 
+            this.msgpdf_btn.Location = new System.Drawing.Point(12, 27);
+            this.msgpdf_btn.Name = "msgpdf_btn";
+            this.msgpdf_btn.Size = new System.Drawing.Size(269, 23);
+            this.msgpdf_btn.TabIndex = 0;
+            this.msgpdf_btn.Text = "Create Messag Pdf File";
+            this.msgpdf_btn.UseVisualStyleBackColor = true;
+            this.msgpdf_btn.Click += new System.EventHandler(this.msgpdf_btn_Click);
+            // 
+            // GroupsDataGridView
+            // 
+            this.GroupsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GroupsDataGridView.Location = new System.Drawing.Point(1, 192);
+            this.GroupsDataGridView.Name = "GroupsDataGridView";
+            this.GroupsDataGridView.Size = new System.Drawing.Size(292, 203);
+            this.GroupsDataGridView.TabIndex = 1;
+            // 
+            // data_btn
+            // 
+            this.data_btn.Location = new System.Drawing.Point(12, 163);
+            this.data_btn.Name = "data_btn";
+            this.data_btn.Size = new System.Drawing.Size(269, 23);
+            this.data_btn.TabIndex = 2;
+            this.data_btn.Text = "Get Data To PDF";
+            this.data_btn.UseVisualStyleBackColor = true;
+            this.data_btn.Click += new System.EventHandler(this.data_btn_Click);
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(383, 62);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(192, 333);
+            this.axAcroPDF1.TabIndex = 3;
+            // 
+            // readpdf_btn
+            // 
+            this.readpdf_btn.Location = new System.Drawing.Point(413, 27);
+            this.readpdf_btn.Name = "readpdf_btn";
+            this.readpdf_btn.Size = new System.Drawing.Size(140, 23);
+            this.readpdf_btn.TabIndex = 4;
+            this.readpdf_btn.Text = "Read Pdf File";
+            this.readpdf_btn.UseVisualStyleBackColor = true;
+            this.readpdf_btn.Click += new System.EventHandler(this.readpdf_btn_Click);
+            // 
+            // TextPDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 396);
+            this.Controls.Add(this.readpdf_btn);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.data_btn);
+            this.Controls.Add(this.GroupsDataGridView);
+            this.Controls.Add(this.msgpdf_btn);
+            this.Name = "TextPDF";
+            this.Text = "TextPDF";
+            this.Load += new System.EventHandler(this.TextPDF_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GroupsDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button msgpdf_btn;
+        private System.Windows.Forms.DataGridView GroupsDataGridView;
+        private System.Windows.Forms.Button data_btn;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.Button readpdf_btn;
+    }
+}
---
+++ Learn ItextSharp/TPITextSharp/TPITextSharp/TextPDF.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using TPITextSharp.Data.DAL;
+using TPITextSharp.Data.BLL;
+using App.Gwin.Entities.Secrurity.Authentication;
+using App.Gwin;
+using StockManagement.BAL;
+using GenericWinForm.Demo;
+
+namespace TPITextSharp
+{
+    public partial class TextPDF : Form
+    {
+        public TextPDF()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// using itextsharp :
+        /// 1 - Create Message
+        /// 2 - Create Lists
+        /// 3 - Working with images
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void msgpdf_btn_Click(object sender, EventArgs e)
+        {
+            // 1
+            Document doc = new Document(PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream("MessagTest.pdf", FileMode.Create));
+            doc.Open();
+            Paragraph paragraph = new Paragraph("Testing Itextsharp !!");
+            doc.Add(paragraph);
+            // 2
+            List list = new List(List.UNORDERED);
+            list.Add(new ListItem("One"));
+            list.Add("Two");
+            list.Add("Three");
+            list.Add("four");
+            doc.Add(list);
+            // 3
+            iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(@"C:\Users\DELL\Desktop\Learn ItextSharp\TPITextSharp\TPITextSharp\Images\Lighthouse.jpg");
+            //image.ScalePercent(20f);
+            //image.SetAbsolutePosition(doc.PageSize.Width - 36f - 72f, doc.PageSize.Height - 36f - 216.6f);
+            image.ScaleToFit(50f, 100f);
+            image.Border = iTextSharp.text.Rectangle.BOX;
+            image.BorderColor = iTextSharp.text.BaseColor.YELLOW;
+            image.BorderWidth = 5f;
+            doc.Add(image);
+            
+
+            doc.Close();
+        }
+
+        /// <summary>
+        /// Get Data From DataGridview to pdf
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void data_btn_Click(object sender, EventArgs e)
+        {
+            Document doc = new Document(PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream("TableTest.pdf", FileMode.Create));
+            doc.Open();
+
+
+            PdfPTable table = new PdfPTable(GroupsDataGridView.Columns.Count);
+            // add the headers from the dgv to the table
+            for (int i = 0; i < GroupsDataGridView.Columns.Count; i++)
+            {
+                table.AddCell(new Phrase(GroupsDataGridView.Columns[i].HeaderText));
+            }
+            // flag the first row as a header
+            table.HeaderRows = 1;
+
+            // add the actual rows from the dgv to the table
+            for (int i = 0; i < GroupsDataGridView.Rows.Count; i++)
+            {
+                for (int j = 0; j < GroupsDataGridView.Columns.Count; j++)
+                {
+                    if(GroupsDataGridView[j,i].Value != null)
+                    {
+                        table.AddCell(new Phrase(GroupsDataGridView[j, i].Value.ToString()));
+                    }
+                }
+            }
+
+            doc.Add(table);
+            doc.Close();
+        }
+
+        // 
+        ModelContext db = new ModelContext();
+        private void TextPDF_Load(object sender, EventArgs e)
+        {
+            User user = null;
+            //user = User.CreateGuestUser(new ModelContext());
+
+            user = User.CreateAdminUser(new ModelContext());
+
+            // user = User.CreateRootUser(new ModelContext());
+            // Start Gwin Application with Authentification
+            FormMenuApplication form = new FormMenuApplication();
+            GwinApp.Start(typeof(ModelContext), typeof(BaseBLO<>), form, user);
+
+            //
+            GroupsDataGridView.DataSource = null;
+            GroupsDataGridView.DataSource = new GroupBLO(db).GetAll();
+            //
+            GroupsDataGridView.Columns.Remove("Id");
+            GroupsDataGridView.Columns.Remove("Ordre");
+            GroupsDataGridView.Columns.Remove("DateCreation");
+            GroupsDataGridView.Columns.Remove("DateModification");
+            GroupsDataGridView.Columns.Remove("Reference");
+        }
+
+        /// <summary>
+        /// get pdf file to the form using reader pdf itextsharp
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void readpdf_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                axAcroPDF1.src = file.FileName;
+            }
+        }
+    }
+}
