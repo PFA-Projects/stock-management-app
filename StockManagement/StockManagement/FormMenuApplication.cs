1f53ac178d7a058a328a12cbd6c605a05a7c6467
ESSARRAJ <essarraj.fouad@gmail.com>
2017-05-09 14:39:23 +0100

    commit

---
+++ StockManagement/StockManagement/FormMenuApplication.cs
@@ -0,0 +1,54 @@
+﻿using App.Gwin;
+using App.Gwin.Application.Presentation.MainForm;
+using App.Gwin.Entities.Secrurity.Authentication;
+using StockManagement.BAL;
+using StockManagement.DAL;
+using System;
+
+namespace GenericWinForm.Demo
+{
+    public partial class FormMenuApplication : FormApplication
+    {
+        public FormMenuApplication()
+        {
+           // GwinApp.Loading_Status("Creating Application Menu...");
+            InitializeComponent();
+         
+        }
+
+        private void FormMenuApplication_Load(object sender, EventArgs e)
+        {
+            User user = null;
+            //user = User.CreateGuestUser(new ModelContext());
+
+            user = User.CreateAdminUser(new ModelContext());
+            user.Language = GwinApp.Languages.fr;
+
+            // user = User.CreateRootUser(new ModelContext());
+            // Start Gwin Application with Authentification
+            GwinApp.Start(typeof(ModelContext), typeof(BaseBLO<>), this, user);
+        }
+
+     
+
+        
+
+
+
+
+
+
+
+
+        ///// <summary>
+        ///// Reload the form after language change
+        ///// </summary>
+        //public override void Reload()
+        //{
+        //    base.Reload();
+        //    InitializeComponent();
+        //}
+
+
+    }
+}
